192df0eba998089da4adc08473f7115e4e2ad438
Your Name <you@example.com>
2024-08-16 08:35:29 -0400

    add features

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,24 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(books[parseInt(req.params.isbn)]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let results = {};
+  for (isbn in books) {
+      if (books[isbn].author == req.params.author) {
+          results[isbn] = books[isbn];
+      }
+  }
+  return res.status(300).json(results);
 });
 
 // Get all books based on title
